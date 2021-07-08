ffe546e58b60a8b82a14a2532d56a503a4645e77
Amari James SEI <73178321+marjames98@users.noreply.github.com>
2021-07-08 13:46:36 -0400

    Initial commit

---
+++ src/AboutMe.js
@@ -0,0 +1,29 @@
+const aboutMe =
+<html>
+<body>
+
+<br></br>
+<br></br>
+
+<img class="img1" src="https://scontent-atl3-2.xx.fbcdn.net/v/t1.6435-9/186297485_1104822086706409_5390037232218721306_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=zeJbZoCWOC0AX8AqIMQ&_nc_oc=AQnYYbwCkUMxhTdksfqF0Ipib-Lb9Jgsrb5bM9DXSzOxLx0s7Jdqe8KpAG8qM2hpgTUPB4bfj36AeQyIqahtchvb&_nc_ht=scontent-atl3-2.xx&oh=db994c233977552f13e1fc64e5ec03a9&oe=60D37FBC"></img>
+
+
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+
+<h1>Amari James</h1> 
+<p> Specializing in adaptive, polished, and prompt professional software development.</p>
+<p><em>Click <a target="_blank" href="https://docs.google.com/document/d/e/2PACX-1vSlAmPLslV1XgLziz6xSllcTpQcchXG85MUm8HxRY4UY32GM3XbncFDTLVYn8XKeTuoln0gWm2e-Dlr/pub"><strong>here</strong></a> to view resume!</em></p>
+
+<script>
+
+document.getElementById("demo").innerHTML = 5 + 6;
+
+</script>
+
+</body>
+</html>
+
+export default aboutMe;
