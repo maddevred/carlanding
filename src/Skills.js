87f43958624f7411ea7e2922d982c2beb4c3678e
Amari James <mar.james1998@gmail.com>
2021-07-10 11:10:15 -0400

    testimonialsdone

--- src/Skills.js
+++ src/Skills.js
@@ -10,19 +10,30 @@ const skills =
 <img class="pic3" src="https://di-uploads-pod6.dealerinspire.com/siddillonautogroup/uploads/2019/04/Interior-Car-Detailing_155243868.jpg"></img>
 
 <br></br>
-
+<br></br>
+<br></br>
 <img class="pic4" src="https://cardetailing.ca/wp-content/uploads/2020/06/services-01.jpg"></img>
 
 <img class="pic5" src="https://www.detailxperts.net/wp-content/uploads/2017/07/Exterior-Car-Detailing-Everything-You-Need-to-Know.jpg"></img>
 
 <img class="pic6" src="https://s25180.pcdn.co/wp-content/uploads/2018/08/iStock-543601188.jpg"></img>
-
 <br></br>
-<p1>"They did a wonderful job detailing my car. My car feels like new again. I certainly will be using ECleanz again, and I highly recommend you give them a shot."</p1>
 <br></br>
-<p2>"They did awesome. I've had my truck for over 5 years and it's always been outside so the paint was pretty rough. They made it look really good, fixed a lot of dull areas, cleaned up minor scratches, and made the inside look brand new."</p2>
 <br></br>
-<p3>" I cannot say enough good things about TrendSettas. The owner is friendly, thoughtful and professional. The courteous employees take pride in their work and it shows. I took my car to be detailed and it felt like I left with a brand new car.   I will definitely return."</p3>
+<br></br>
+<p1>"They did a wonderful job detailing my car. My car feels like new again. I certainly will be using ECleanz again, and I highly recommend you give them a shot." - Customer Jones</p1>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<p2>"They did awesome. I've had my truck for over 5 years and it's always been outside so the paint was pretty rough. They made it look really good, fixed a lot of dull areas, cleaned up minor scratches, and made the inside look brand new." - Customer James</p2>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<p3>" I cannot say enough good things about ECleanz. The owner is friendly, thoughtful and professional. The courteous employees take pride in their work and it shows. I took my car to be detailed and it felt like I left with a brand new car. I will definitely return." - Customer Carter </p3>
 <script>
 document.getElementById("demo").innerHTML = 5 + 6;
 </script>
